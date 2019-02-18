39bf0e8f0e3ac13c40eb84b638b031a8b89e4867
Eran Shapira <lxeran@gmail.com>
2019-02-18 14:34:58 +0200

    add server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let branchVersion = undefined;
+app.get('/setVersion', (req, res) => {
+    branchVersion = req.query.branchVersion;
+    res.send(JSON.stringify({
+        branchVersion
+    }));
+});
+app.get('/', (req, res) => res.send(`
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Webpack App</title>
+  </head>
+  <body>
+  <script type="text/javascript" src="${branchVersion || 'http://localhost:8080/'}main.js"></script></body>
+</html>
+`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
